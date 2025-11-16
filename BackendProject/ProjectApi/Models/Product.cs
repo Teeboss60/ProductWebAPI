36868188be7d222e6b958d7ee09502b4c3c3e151
Teeboss60 <78246538+Teeboss60@users.noreply.github.com>
2025-11-16 10:45:08 +0700

    Add files via upload

---
+++ BackendProject/ProjectApi/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductID { get; set; } = string.Empty;
+    }
+}
---
+++ BackendProject/ProjectApi/Models/QRCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectAPI.Models
+{
+    public class QRCodes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string QRImage { get; set; } = string.Empty;
+    }
+}
